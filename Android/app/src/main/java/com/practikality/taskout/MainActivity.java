f25411e42accab95320e25d52e807fdca7f3dc59
padamchopra <jb.padamchopra@gmail.com>
2019-05-22 15:26:38 +0530

    teaser final

--- Android/app/src/main/java/com/practikality/taskout/MainActivity.java
+++ Android/app/src/main/java/com/practikality/taskout/MainActivity.java
@@ -1,13 +1,82 @@
 package com.practikality.taskout;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user!=null){
+            Toast.makeText(this, "User signed in already.", Toast.LENGTH_SHORT).show();
+        }
     }
+/*
+    public void signup(View view){
+        String useremail = ((EditText) findViewById(R.id.useremail)).getText().toString();
+        String userpass = ((EditText) findViewById(R.id.userpassword)).getText().toString();
+        mAuth.createUserWithEmailAndPassword(useremail, userpass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, user.getUid(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Sign up failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    public void login(View view){
+        String useremail = ((EditText) findViewById(R.id.useremail)).getText().toString();
+        String userpass = ((EditText) findViewById(R.id.userpassword)).getText().toString();
+        mAuth.signInWithEmailAndPassword(useremail, userpass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Signed in: " + user.getUid(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Sign in failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void logout(View view){
+        mAuth.signOut();
+    }*/
 }
--- Web/functions/index.js
+++ Web/functions/index.js
@@ -2,18 +2,21 @@ const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
 const express = require('express');
 const engines = require('consolidate');
-
+const app = express();
 const firebaseApp = firebase.initializeApp(
     functions.config().firebase
 );
-
-const app = express();
 app.engine('hbs',engines.handlebars);
 app.set('views','./views');
 app.set('view engine','hbs');
-
 app.use(express.urlencoded());
 app.use(express.json());
+
+
+//Subscribed Route
+app.get('/subscribed', (req, res, next) => {
+    res.redirect('../');
+})
 
 app.post('/subscribed', (req, res, next) => {
     const emailid = req.body.email;
@@ -30,11 +33,11 @@ app.post('/subscribed', (req, res, next) => {
     });
 });
 
+
+//Default Route
 app.get('/', (req, res, next) => {
     res.render('index');
 });
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
+//Export app
 exports.app = functions.https.onRequest(app);

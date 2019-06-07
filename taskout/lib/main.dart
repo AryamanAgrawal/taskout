e10c25041bdb30e45be908d0bf1c8529aa0baa87
padamchopra <jb.padamchopra@gmail.com>
2019-06-07 14:18:23 +0530

    v0.1 flutter skeleton

---
+++ taskout/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello Aryaman"),
+        ),
+      ),
+    );
+  }
+}
